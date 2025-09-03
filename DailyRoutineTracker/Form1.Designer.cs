92b0c08d5f02d90c882ec00c440a9b890bc45e1d
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2025-09-03 20:58:16 +0700

    Add project files.

---
+++ DailyRoutineTracker/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace DailyRoutineTracker
+{
+    partial class home
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            btntambah = new Button();
+            label1 = new Label();
+            pleft = new Panel();
+            flpActions = new FlowLayoutPanel();
+            txtbactivity = new TextBox();
+            lblkegiatan = new Label();
+            dtptime = new DateTimePicker();
+            lblwkt = new Label();
+            dtpdate = new DateTimePicker();
+            lbltngl = new Label();
+            btnhps = new Button();
+            pright = new Panel();
+            pSearchBar = new Panel();
+            btnjson = new Button();
+            dgv = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            pleft.SuspendLayout();
+            flpActions.SuspendLayout();
+            pright.SuspendLayout();
+            pSearchBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgv).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(462, 84);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(0, 0);
+            dataGridView1.TabIndex = 3;
+            // 
+            // btntambah
+            // 
+            btntambah.BackColor = Color.Red;
+            btntambah.FlatAppearance.BorderSize = 0;
+            btntambah.FlatStyle = FlatStyle.Flat;
+            btntambah.ForeColor = Color.White;
+            btntambah.Location = new Point(3, 3);
+            btntambah.Name = "btntambah";
+            btntambah.Size = new Size(110, 34);
+            btntambah.TabIndex = 4;
+            btntambah.Text = "Tambah";
+            btntambah.UseVisualStyleBackColor = false;
+            btntambah.Click += btntambah_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label1.Location = new Point(178, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(335, 41);
+            label1.TabIndex = 5;
+            label1.Text = "Daily Routine Tracker  ";
+            label1.Click += label1_Click;
+            // 
+            // pleft
+            // 
+            pleft.BorderStyle = BorderStyle.FixedSingle;
+            pleft.Controls.Add(flpActions);
+            pleft.Controls.Add(txtbactivity);
+            pleft.Controls.Add(lblkegiatan);
+            pleft.Controls.Add(dtptime);
+            pleft.Controls.Add(lblwkt);
+            pleft.Controls.Add(dtpdate);
+            pleft.Controls.Add(lbltngl);
+            pleft.ForeColor = Color.Black;
+            pleft.ImeMode = ImeMode.NoControl;
+            pleft.Location = new Point(20, 84);
+            pleft.Name = "pleft";
+            pleft.Size = new Size(340, 205);
+            pleft.TabIndex = 11;
+            pleft.Paint += pleft_Paint;
+            // 
+            // flpActions
+            // 
+            flpActions.BorderStyle = BorderStyle.FixedSingle;
+            flpActions.Controls.Add(btntambah);
+            flpActions.Location = new Point(3, 148);
+            flpActions.Margin = new Padding(0, 8, 0, 0);
+            flpActions.Name = "flpActions";
+            flpActions.Size = new Size(120, 43);
+            flpActions.TabIndex = 13;
+            // 
+            // txtbactivity
+            // 
+            txtbactivity.BorderStyle = BorderStyle.FixedSingle;
+            txtbactivity.Location = new Point(69, 68);
+            txtbactivity.Multiline = true;
+            txtbactivity.Name = "txtbactivity";
+            txtbactivity.Size = new Size(251, 69);
+            txtbactivity.TabIndex = 13;
+            // 
+            // lblkegiatan
+            // 
+            lblkegiatan.AutoSize = true;
+            lblkegiatan.Location = new Point(3, 68);
+            lblkegiatan.Name = "lblkegiatan";
+            lblkegiatan.Size = new Size(60, 20);
+            lblkegiatan.TabIndex = 16;
+            lblkegiatan.Text = "Kegitan";
+            // 
+            // dtptime
+            // 
+            dtptime.CustomFormat = "HH : mm";
+            dtptime.Format = DateTimePickerFormat.Custom;
+            dtptime.Location = new Point(70, 34);
+            dtptime.Name = "dtptime";
+            dtptime.ShowUpDown = true;
+            dtptime.Size = new Size(250, 27);
+            dtptime.TabIndex = 15;
+            dtptime.ValueChanged += dtptime_ValueChanged;
+            // 
+            // lblwkt
+            // 
+            lblwkt.AutoSize = true;
+            lblwkt.Location = new Point(3, 34);
+            lblwkt.Name = "lblwkt";
+            lblwkt.Size = new Size(50, 20);
+            lblwkt.TabIndex = 14;
+            lblwkt.Text = "Waktu";
+            lblwkt.Click += lblwkt_Click;
+            // 
+            // dtpdate
+            // 
+            dtpdate.Location = new Point(70, 3);
+            dtpdate.Name = "dtpdate";
+            dtpdate.Size = new Size(250, 27);
+            dtpdate.TabIndex = 13;
+            // 
+            // lbltngl
+            // 
+            lbltngl.AutoSize = true;
+            lbltngl.Location = new Point(3, 3);
+            lbltngl.Name = "lbltngl";
+            lbltngl.Size = new Size(61, 20);
+            lbltngl.TabIndex = 13;
+            lbltngl.Text = "Tanggal";
+            lbltngl.Click += lbltngl_Click;
+            // 
+            // btnhps
+            // 
+            btnhps.BackColor = Color.Red;
+            btnhps.FlatAppearance.BorderSize = 0;
+            btnhps.FlatStyle = FlatStyle.Flat;
+            btnhps.ForeColor = Color.White;
+            btnhps.Location = new Point(253, 403);
+            btnhps.Name = "btnhps";
+            btnhps.Size = new Size(110, 34);
+            btnhps.TabIndex = 5;
+            btnhps.Text = "Hapus";
+            btnhps.UseVisualStyleBackColor = false;
+            // 
+            // pright
+            // 
+            pright.BorderStyle = BorderStyle.FixedSingle;
+            pright.Controls.Add(btnhps);
+            pright.Controls.Add(pSearchBar);
+            pright.Controls.Add(dgv);
+            pright.Location = new Point(550, 0);
+            pright.Name = "pright";
+            pright.Size = new Size(377, 450);
+            pright.TabIndex = 12;
+            pright.Paint += pright_Paint;
+            // 
+            // pSearchBar
+            // 
+            pSearchBar.BackColor = Color.White;
+            pSearchBar.BorderStyle = BorderStyle.FixedSingle;
+            pSearchBar.Controls.Add(btnjson);
+            pSearchBar.Dock = DockStyle.Top;
+            pSearchBar.Location = new Point(0, 0);
+            pSearchBar.Name = "pSearchBar";
+            pSearchBar.Size = new Size(375, 44);
+            pSearchBar.TabIndex = 0;
+            // 
+            // btnjson
+            // 
+            btnjson.Location = new Point(228, 3);
+            btnjson.Name = "btnjson";
+            btnjson.Size = new Size(142, 29);
+            btnjson.TabIndex = 1;
+            btnjson.Text = "Export JSON";
+            btnjson.UseVisualStyleBackColor = true;
+            // 
+            // dgv
+            // 
+            dgv.BackgroundColor = Color.FromArgb(224, 224, 224);
+            dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv.Dock = DockStyle.Fill;
+            dgv.Location = new Point(0, 0);
+            dgv.Name = "dgv";
+            dgv.RowHeadersWidth = 51;
+            dgv.Size = new Size(375, 448);
+            dgv.TabIndex = 13;
+            // 
+            // home
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(926, 450);
+            Controls.Add(pright);
+            Controls.Add(pleft);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "home";
+            Text = "Home";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            pleft.ResumeLayout(false);
+            pleft.PerformLayout();
+            flpActions.ResumeLayout(false);
+            pright.ResumeLayout(false);
+            pSearchBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgv).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private DataGridView dataGridView1;
+        private Button btntambah;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Panel pleft;
+        private Label lbltngl;
+        private DateTimePicker dtpdate;
+        private Label lblwkt;
+        private DateTimePicker dtptime;
+        private Label lblkegiatan;
+        private TextBox txtbactivity;
+        private FlowLayoutPanel flpActions;
+        private Button btnhps;
+        private Panel pright;
+        private Panel pSearchBar;
+        private DataGridView dgv;
+        private Button button1;
+        private Button btnjson;
+    }
+}
---
+++ DailyRoutineTracker/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;   // BindingList
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace DailyRoutineTracker
+{
+    public partial class home : Form
+    {
+        // ========= Model =========
+        public class RoutineItem
+        {
+            public Guid Id { get; set; } = Guid.NewGuid();
+            public DateTime Date { get; set; } = DateTime.Today;
+            public int Hour { get; set; }
+            public int Minute { get; set; }
+            public string Activity { get; set; } = "";
+
+            public DateTime At => new DateTime(Date.Year, Date.Month, Date.Day, Hour, Minute, 0);
+            public string Tanggal => At.ToString("dd MMM yyyy");
+            public string Waktu => At.ToString("HH:mm");
+        }
+
+        // ========= State & path =========
+        private BindingList<RoutineItem> _items = new BindingList<RoutineItem>();
+        private readonly BindingSource _bs = new BindingSource();
+
+        private readonly string _folder =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                         "DailyRoutineTracker");
+        private string FilePath => Path.Combine(_folder, "routines.json");
+
+        public home()
+        {
+            InitializeComponent();
+            // Jangan daftar event di sini untuk menghindari duplikasi.
+            // Semua event dihubungkan lewat Designer (Properties → Events).
+        }
+
+        // ========= FORM LOAD =========
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Setup Date / Time picker
+            dtpdate.Format = DateTimePickerFormat.Short;
+
+            dtptime.Format = DateTimePickerFormat.Custom;
+            dtptime.CustomFormat = "HH:mm";
+            dtptime.ShowUpDown = true; // spin-box, tanpa kalender
+
+            // DataGridView setup (tanpa ID, tambah tombol Hapus)
+            dgv.AutoGenerateColumns = false;
+            dgv.AllowUserToAddRows = false;  // hilangkan baris kosong
+            dgv.RowHeadersVisible = false;  // hilangkan header kiri
+            dgv.ReadOnly = true;
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv.MultiSelect = false;
+
+            dgv.Columns.Clear();
+            dgv.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colTanggal",
+                HeaderText = "Tanggal",
+                DataPropertyName = "Tanggal",
+                Width = 120
+            });
+            dgv.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colWaktu",
+                HeaderText = "Waktu",
+                DataPropertyName = "Waktu",
+                Width = 80
+            });
+            dgv.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colActivity",
+                HeaderText = "Kegiatan",
+                DataPropertyName = "Activity",
+                AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+            });
+            dgv.Columns.Add(new DataGridViewButtonColumn
+            {
+                Name = "colDel",
+                HeaderText = "",
+                Text = "Hapus",
+                UseColumnTextForButtonValue = true,
+                Width = 70
+            });
+
+            // Binding
+            _bs.DataSource = _items;
+            dgv.DataSource = _bs;
+
+            // Load JSON
+            Directory.CreateDirectory(_folder);
+            var loaded = LoadFromDisk();
+            _items = new BindingList<RoutineItem>(loaded.OrderBy(i => i.At).ToList());
+            _bs.DataSource = _items;
+            dgv.DataSource = _bs;
+
+            // Default input
+            dtpdate.Value = DateTime.Today;
+            dtptime.Value = DateTime.Today.AddHours(8);
+        }
+
+        // ========= FORM CLOSING =========
+        private void Home_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveToDisk();
+        }
+
+        // ========= TAMBAH / SIMPAN =========
+        private void btntambah_Click(object sender, EventArgs e)
+        {
+            var activity = GetActivityText(); // validasi bersih
+
+            if (string.IsNullOrEmpty(activity))
+            {
+                MessageBox.Show("Isi 'Kegiatan' dulu ya.", "Info",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            var item = new RoutineItem
+            {
+                Date = dtpdate.Value.Date,
+                Hour = dtptime.Value.Hour,
+                Minute = dtptime.Value.Minute,
+                Activity = activity
+            };
+
+            _items.Add(item);
+            Resort();
+            SaveToDisk();
+            ClearInputs();
+        }
+
+        // ========= HAPUS (tombol umum) =========
+        private void btnhps_Click(object sender, EventArgs e)
+        {
+            if (dgv.CurrentRow?.DataBoundItem is RoutineItem sel)
+            {
+                if (ConfirmDelete(sel))
+                {
+                    _items.Remove(sel);
+                    SaveToDisk();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pilih item pada daftar dulu.", "Info",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // ========= HAPUS (tombol per baris di kanan) =========
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+            if (dgv.Columns[e.ColumnIndex].Name != "colDel") return;
+
+            if (dgv.Rows[e.RowIndex].DataBoundItem is RoutineItem sel && ConfirmDelete(sel))
+            {
+                _items.Remove(sel);
+                SaveToDisk();
+            }
+        }
+
+        private bool ConfirmDelete(RoutineItem sel)
+        {
+            var ok = MessageBox.Show(
+                $"Hapus '{sel.Activity}' ({sel.At:dd MMM yyyy HH:mm})?",
+                "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            return ok == DialogResult.Yes;
+        }
+
+        // ========= EXPORT JSON (btnjson) =========
+        private void btnjson_Click(object sender, EventArgs e)
+        {
+            using var sfd = new SaveFileDialog
+            {
+                Filter = "JSON Files (*.json)|*.json",
+                FileName = $"Routine_{DateTime.Now:yyyyMMdd_HHmm}.json"
+            };
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var list = _items.OrderBy(i => i.At).ToList();
+                    var json = JsonSerializer.Serialize(list,
+                        new JsonSerializerOptions { WriteIndented = true });
+                    File.WriteAllText(sfd.FileName, json);
+                    MessageBox.Show("Berhasil diexport.", "Sukses",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Gagal export: {ex.Message}", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // ========= SAVE / LOAD =========
+        private void SaveToDisk()
+        {
+            try
+            {
+                Directory.CreateDirectory(_folder);
+                var json = JsonSerializer.Serialize(_items.ToList(),
+                    new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(FilePath, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Gagal menyimpan: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private List<RoutineItem> LoadFromDisk()
+        {
+            try
+            {
+                if (!File.Exists(FilePath)) return new List<RoutineItem>();
+                var json = File.ReadAllText(FilePath);
+                return JsonSerializer.Deserialize<List<RoutineItem>>(json) ?? new List<RoutineItem>();
+            }
+            catch
+            {
+                return new List<RoutineItem>();
+            }
+        }
+
+        // ========= Helper =========
+        private void Resort()
+        {
+            var ordered = _items.OrderBy(i => i.At).ToList();
+            _items = new BindingList<RoutineItem>(ordered);
+            _bs.DataSource = _items;
+            dgv.DataSource = _bs;
+        }
+
+        private void ClearInputs()
+        {
+            dtpdate.Value = DateTime.Today;
+            dtptime.Value = DateTime.Today.AddHours(8);
+            txtbactivity.Text = "";
+        }
+
+        private string GetActivityText()
+        {
+            var s = txtbactivity.Text ?? "";
+            s = s.Replace("\u200B", "")    // zero-width space
+                 .Replace("\u00A0", " ");  // non-breaking space
+            return s.Trim();
+        }
+
+        // ========= Handler kosong yang kamu miliki (tetap dipertahankan) =========
+        private void button1_Click(object sender, EventArgs e) { }        // tidak dipakai
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label4_Click(object sender, EventArgs e) { }
+        private void lbltngl_Click(object sender, EventArgs e) { }
+        private void lblwkt_Click(object sender, EventArgs e) { }
+        private void dtptime_ValueChanged(object sender, EventArgs e) { }
+        private void tlpLeft_Paint(object sender, PaintEventArgs e) { }
+        private void pright_Paint(object sender, PaintEventArgs e) { }
+        private void txtbactivity_TextChanged(object sender, EventArgs e) { }
+        private void dtpdate_ValueChanged(object sender, EventArgs e) { }
+
+        private void pleft_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
